78b42e8fd6caad629ca15cc63d0a5257faddb878
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 22:41:00 +0300

    add controls to form

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -39,7 +39,7 @@ const Form = () => {
        <Grid item xs ={6}>
          <Textfield  type= "date" label ="Date" fullWidth/>
        </Grid>
-       <Button className={classes.button} variant="outlined" color ="primary"> </Button>
+       <Button className={classes.button} variant="outlined" color ="primary" fullWidth> </Button>
 
    </Grid>
   )
