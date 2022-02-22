7c8e5854c197839291d9bf6af42028f7b488ca92
timothyAgevi <timothyagevi@gmail.com>
2022-02-23 00:51:06 +0300

    add context logic

--- src/context/context.js
+++ src/context/context.js
@@ -7,6 +7,8 @@ export const ExpenseTrackerContext = createContext(initialState);
 
 //functional Component
  export const Provider = ({ children})=>{
+//useReducer hook i.e complex version of useState
+
    //return jsx 
    return(
        <ExpenseTrackerContext.Provider value ={{appName:'Expense Tracker'}}>
