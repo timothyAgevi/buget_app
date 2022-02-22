79bb3d4754ce7d5c5b6fb565c708a552b46d48bc
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 23:05:34 +0300

    add List

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -8,26 +8,14 @@ import useStyles from './styles';
 const List = () => {
   const classes = useStyles();
 //   const { transactions, deleteTransaction } = useContext(ExpenseTrackerContext);
-
+ const transactions = []
   return (
-    <MUIList dense={false} className={classes.list}>
-      {transactions.map((transaction) => (
-        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
-                <MoneyOff />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
-                <Delete />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        </Slide>
-      ))}
+    <MUIList>
+        {transactions.map((transaction)=>(
+            <Slide>
+
+            </Slide>
+        ))}
     </MUIList>
   );
 };
