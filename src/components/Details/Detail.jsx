d654aac072589cb20b43085db8bdebf88ff802c8
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 18:33:18 +0300

    add material imports to details compoinent

---
+++ src/components/Details/Detail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Card,CardHeader,CardContent,Typography }from '@material-ui/core'
+const Detail = () => {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
