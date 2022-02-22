4043efc589ae99b1d46d0646bd67f27f2f16063a
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 22:24:39 +0300

    add styles to form

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -5,7 +5,23 @@ import { Textfiels,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem
 const Form = () => {
     const classes =useStyles();
   return (
-   
+   <Grid container spacing ={2}>
+     <Grid items xs={12}> 
+     <Typography align ="centre" variant ="subtitle2"gutterBottom>
+          ...
+     </Typography>
+     </Grid>
+
+     <Grid item xs ={6}>
+       <FormControl fullWidth>
+        <InputLabel> Type </InputLabel>
+        <Select>
+            <MenuItem value ="Income">Income</MenuItem>
+            <MenuItem value ="Expense">Expense</MenuItem>
+        </Select>
+       </FormControl>
+     </Grid>
+   </Grid>
   )
 }
 
