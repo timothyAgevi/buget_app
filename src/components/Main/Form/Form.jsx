300cd0f24fbcfc8ac44bbc46716442ecfa23e9b3
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 22:07:08 +0300

    add styles to form

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Textfiels,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem} from '@material-ui/core'
+ import useStyles from './styles'
+
 const Form = () => {
+    const classes =useStyles();
   return (
     <div>Form</div>
   )
