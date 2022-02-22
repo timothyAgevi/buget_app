98a1fc7952dbfb8ee99d8e529712e44105a6f104
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 23:34:13 +0300

    add controls to List

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -24,7 +24,7 @@ const List = () => {
                   <ListItemText  primary ={transaction.category} secondary ={ `$${transaction.amount}  - ${transaction.date}`  }/>
                   <ListItemSecondaryAction>
                       <IconButton edge ="end" aria-label='delete' onClick="">
-
+                         <Delete/>
                       </IconButton>
                   </ListItemSecondaryAction>
               </ListItem>
