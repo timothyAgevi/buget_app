88dfbd93453739aabf7e06af0bdba13fac57af00
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 20:57:15 +0300

    add style.js to App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
       <div>
-   <Grid  container spacing={0} alignItems="centre" justify ="centre" style={{height:'100vh'}}>
+   <Grid  className={classes.grid} container spacing={0} alignItems="centre" justify ="centre" style={{height:'100vh'}}>
        <Grid item xs= {12} sm={4}>
            <Details/>
        </Grid>
