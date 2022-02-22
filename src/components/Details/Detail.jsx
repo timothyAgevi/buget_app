be9009701b516bb14f11bcd60321aa3f6d06009e
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 18:59:35 +0300

    add card

--- src/components/Details/Detail.jsx
+++ src/components/Details/Detail.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Card,CardHeader,CardContent,Typography }from '@material-ui/core'
-const Detail = () => {
+import { Card,CardHeader,CardContent,Typography }from '@material-ui/core';
+import {Doughnut} from'react-chartjs-2'
+;const Detail = () => {
   return (
     <Card>
         <CardHeader title="Income"/>
         <CardContent>
             <Typography variant='h5'> $50</Typography>
+            <Doughnut data ="Data"/>
         </CardContent>
     </Card>
   )
