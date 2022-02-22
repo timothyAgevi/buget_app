6c7cc7cf807689d1b546f705a3cbe0caea56fcde
timothyAgevi <timothyagevi@gmail.com>
2022-02-23 01:23:22 +0300

    add context logic

--- src/context/context.js
+++ src/context/context.js
@@ -13,11 +13,14 @@ const [transactions, dispatch] = useReducer(contextReducer,initialState);//reduc
 //Actions/action creators
  const deleteTransaction =(id)=>{dispatch({type:'DELETE_TRANSACTION',payload:id}) }
 
- const addTransaction= ()=>{dispatch({ type:'ADD_TRASACTION',payload:transaction});
+ const addTransaction= ()=>{dispatch({ type:'ADD_TRASACTION',payload:transactions});//err transactions
  }
    //return jsx 
    return(
-       <ExpenseTrackerContext.Provider value ={{appName:'Expense Tracker'}}>
+       <ExpenseTrackerContext.Provider value ={{
+        deleteTransaction,
+        addTransaction
+       }}>
            {children}
        </ExpenseTrackerContext.Provider>
    )
