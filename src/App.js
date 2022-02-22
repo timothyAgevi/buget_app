d2ab2c4391e3d52bc6d79753612e18cd990254d6
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 22:56:08 +0300

    add List

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ const App = () => {
   return (
       <div>
    <Grid  className={classes.grid} container spacing={0} alignItems="centre" justify ="centre" style={{height:'100vh'}}>
-       <Grid item xs= {12} sm={4}>
+       <Grid item xs= {12} sm={3}>
            <Details title ="Income"/>
        </Grid>
 
-       <Grid item xs= {12} sm={3}>
+       <Grid item xs= {12} sm={4}>
           < Main/>
        </Grid>
 
-       <Grid item xs= {12} sm={4}>
+       <Grid item xs= {12} sm={3}>
            <Details title ="Expense"/>
        </Grid>
    </Grid>
