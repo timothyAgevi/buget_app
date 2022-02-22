5acb4af2953734c16c552f7900df398e192b897f
timothyAgevi <timothyagevi@gmail.com>
2022-02-23 00:07:09 +0300

    add context

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,9 @@
 import React,{ userReducer,createContext}from 'react';
 
 const initialState = [];
- export const ExpenseTrackerContext = createContext(initailState);
+
+export const ExpenseTrackerContext = createContext(initailState);
+
+//functional Component
+ export const Provider = ({ children})
 
