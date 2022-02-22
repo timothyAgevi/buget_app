280393397ca4473f41d4906798c8dc0ed47a8bdd
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 22:04:11 +0300

    add form to Main Component

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Textfiels,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem} from '@material-ui/core'
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
