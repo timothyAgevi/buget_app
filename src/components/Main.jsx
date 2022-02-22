6537d26c6b74edb2316043ea23e8f343527bdacb
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 21:18:35 +0300

    add Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
+const Main = () => {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
