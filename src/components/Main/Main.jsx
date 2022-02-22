c25a4923f523f75af8823510a29c19a1e54e8e7e
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 23:35:18 +0300

    add controls to List

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
 import useStyles from './styles';
 import Form from './Form/Form'
+import List from './List/List';
 
 const Main = () => {
     const classes =useStyles();
@@ -20,7 +21,7 @@ const Main = () => {
    <CardContent className={classes.CardContent}>
        <Grid container spacing ={2}>
            <Grid item xs={12}>
-            {/* <List/> */}
+            <List/>
            </Grid>
 
        </Grid>
