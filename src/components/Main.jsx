0e136f350a1d0e752f453f1e2e3bbb93404bd0a6
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 21:22:09 +0300

    add card header

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
 const Main = () => {
   return (
-
+<Card className={ classes.root}>
+    <CardHeader title = "Buget Manager" subheader= "Powered by Speechly"/>
+</Card>
     )
 }
 
