af947d0500caf509dd52445f16f9ceaf82c0cfe6
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 21:54:24 +0300

    add styles to Main Component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
-import useStyles from './syles'
+import useStyles from './syles';
+
 const Main = () => {
     const classes =useStyles();
   return (
