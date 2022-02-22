b2a4a271f36775ee086db5028f3246aab9cbb311
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 21:19:28 +0300

    delete div from Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
 const Main = () => {
   return (
-    <div>Main</div>
-  )
+
+    )
 }
 
 export default Main
