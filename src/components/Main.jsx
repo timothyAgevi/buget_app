8b7532737d49277bd307ca6523f33463e1b9f5ea
timothyAgevi <timothyagevi@gmail.com>
2022-02-22 21:23:33 +0300

    add styles hooks

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import{Card,CardHeader,CardContent,Typography,Grid,Divider}from '@material-ui/core'
 import useStyles from './syles'
 const Main = () => {
+    const classes =useStyles();
   return (
 <Card className={ classes.root}>
     <CardHeader title = "Buget Manager" subheader= "Powered by Speechly"/>
