df413b57791819596863d323c9b04c3cd78f2284
timothyAgevi <timothyagevi@gmail.com>
2022-02-23 01:43:00 +0300

    add context logic

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,5 +1,9 @@
 // Reducer => a function that takes in old state,and an action=>new state ..
 const contextReducer= (state,action)=>{
+ if( action.type ==='DELETE_TRANSACTION'){
 
+ }else if(action.type==='ADD_TRANSACTION'){
+     
+ }
 }
 export default contextReducer;
