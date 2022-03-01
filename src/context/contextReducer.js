326eb62e0adfbfffc935a255ccd7b305aa6ea395
timothyAgevi <timothyagevi@gmail.com>
2022-03-02 00:01:41 +0300

    define transaction in contextReducer

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,13 +1,15 @@
 // Reducer => a function that takes in old state,and an action=>new state ..
 const contextReducer= (state,action)=>{
+    let transactions;
+
     switch(action.type){
         case 'DELETE_TRANSACTION':
-            const transactions = state.filter(  ( t) =>t.id !== action.payload);
+            transactions = state.filter(  ( t) =>t.id !== action.payload);
             return transactions;
 
         
         case 'ADD_TRANSACTION':
-            const transaction =[action.payload,...state];
+            transaction =[action.payload,...state];
             return transactions;
         default:   
               break;
