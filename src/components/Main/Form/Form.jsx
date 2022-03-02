a529fb2d45ba882acefd989a1d4f7404140b6e16
timothyAgevi <timothyagevi@gmail.com>
2022-03-02 19:52:55 +0300

    import useContext soas to fech

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import { TextField,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem} from '@material-ui/core'
 import { ExpenseTrackerContext } from '../../../context/context'
 import useStyles from './styles'
