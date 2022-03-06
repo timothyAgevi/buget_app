4073356d9acf87acc0fc220c4780d5172c5a0b36
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 22:46:41 +0300

    add custom hook

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -1,3 +1,5 @@
 import { useContext } from "react";
 import { ExpenseTrackerContext } from "./context/context";
 import { incomeCategories,expenseCategories,resetCategories } from "./constants/categories";
+
+//custom hook:arrow function that starts with use
