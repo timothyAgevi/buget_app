d4f7372fef631fd5dbb6949d8b21b11c14936051
timothyAgevi <timothyagevi@gmail.com>
2022-03-07 00:03:27 +0300

    uncomment dougnut

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -11,8 +11,8 @@ const Details = ( {title} ) => {
     <Card className={ title ==='Income' ? classes.income:classes.expense}>
     <CardHeader title = {title}/>
     <CardContent>
-      <Typography variant ="h5">$50</Typography>
-      {/* <Doughnut data ="DATA"/> */}
+      <Typography variant ="h5">{total}</Typography>
+      <Doughnut data ="DATA"/>
     </CardContent>
     </Card>
   )
