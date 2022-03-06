d6e4831356714b8bbeb72a52ff44d9f89a9ca0df
timothyAgevi <timothyagevi@gmail.com>
2022-03-07 01:10:45 +0300

    import SpeechProvider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDom from 'react-dom'
 import App from './App';
 import { Provider } from './context/context';
 import './index.css'
+import { SpeechProvider}from '@speechly/react-client';
 
 ReactDom.render( 
+    <SpeechProvider>
 <Provider>
 <App/>
 </Provider>,
+</SpeechProvider>
  document.getElementById('root'));
