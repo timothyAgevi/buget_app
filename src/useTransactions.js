0035fe4ce2582c9ef05b7a215bf64d1a0cbf2a78
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 22:48:33 +0300

    add parameter to hook

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -3,6 +3,6 @@ import { ExpenseTrackerContext } from "./context/context";
 import { incomeCategories,expenseCategories,resetCategories } from "./constants/categories";
 
 //custom hook:arrow function that starts with use
-const useTransactions=()=>{
-    
+const useTransactions=(title)=>{
+
 }
