5e5337ed3ba827658262722cc9600ebd95e80385
timothyAgevi <timothyagevi@gmail.com>
2022-03-07 00:04:47 +0300

    import Doughnut

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import{Card,CardHeader,CardContent,Typography} from '@material-ui/core'
-// import{Doughnut} from 'react-chartjs-2'
+import{Doughnut} from 'react-chartjs-2'
 import useTransactions from '../../useTransactions'
 import useStyles from "./styles"
 
@@ -12,7 +12,7 @@ const Details = ( {title} ) => {
     <CardHeader title = {title}/>
     <CardContent>
       <Typography variant ="h5">{total}</Typography>
-      <Doughnut data ="DATA"/>
+      <Doughnut data ={charData}/>
     </CardContent>
     </Card>
   )
