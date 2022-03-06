5a7709fe17965dc25da36735fc4269b0a4c3b9ea
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 23:11:35 +0300

    rename selectedCategory to transactionsPerType

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -6,10 +6,10 @@ import { incomeCategories,expenseCategories,resetCategories } from "./constants/
 const useTransactions=(title)=>{
    resetCategories();
    const {transactions}=useContext(ExpenseTrackerContext);
-   const selectedCategories = transactions.filter( (t)=>t.type ===title);
+   const transactionsPerType = transactions.filter( (t)=>t.type ===title);
     // reduce : sum array values to one value
-   const total =selectedCategories.reduce(   (acc,currVal)=>acc+=currVal.amount,0);
+   const total =transactionsPerType.reduce(   (acc,currVal)=>acc+=currVal.amount,0);
   const categories = title==='Income'?incomeCategories:expenseCategories
 
-  console.log({selectedCategories,total,categories});
+  console.log({transactionsPerType,total,categories});
 }
