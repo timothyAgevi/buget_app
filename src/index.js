a8c22b03908492a18a18629811eab4b9428cc7b8
timothyAgevi <timothyagevi@gmail.com>
2022-03-07 01:14:47 +0300

    add language to SpeechProvider

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 import { SpeechProvider}from '@speechly/react-client';
 
 ReactDom.render( 
-    <SpeechProvider appId='07e558a3-d102-4cea-98d5-776b127f4100'>
+    <SpeechProvider appId='07e558a3-d102-4cea-98d5-776b127f4100' language ='en-US'>
 <Provider>
 <App/>
 </Provider>,
