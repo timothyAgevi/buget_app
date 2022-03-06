c123beeaed7ce79dc4bb0e238d5946e764a66eb8
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 23:57:38 +0300

    import useTransaction to Details component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import{Card,CardHeader,CardContent,Typography} from '@material-ui/core'
 // import{Doughnut} from 'react-chartjs-2'
+import useTransactions from '../../useTransactions'
 import useStyles from "./styles"
 
 const Details = ( {title} ) => {
