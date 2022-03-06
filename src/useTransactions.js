51be725128c0f648b8b42b8b86e9ca15e57b542e
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 22:51:57 +0300

    get transactions

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -5,5 +5,6 @@ import { incomeCategories,expenseCategories,resetCategories } from "./constants/
 //custom hook:arrow function that starts with use
 const useTransactions=(title)=>{
    resetCategories();
-   const {transactions}=useContext(ExpenseTrackerContext)
+   const {transactions}=useContext(ExpenseTrackerContext);
+   
 }
