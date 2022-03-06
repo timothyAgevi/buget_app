25a6f89a003e51a1e76d11541023d64cbe05a5df
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 21:50:20 +0300

    add selectedCategories

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -23,6 +23,7 @@ const Form = () => {
       setFormData(initialState);
     }
    // console.log(formData);
+   const selectedCategories=formData.type ==='Income'?incomeCategories:expenseCategories;
   return (
    <Grid container spacing ={2}>
      <Grid items xs={12}> 
