42e5a9e525d10fa430b1ebddfe8cac13f7c1d9ea
timothyAgevi <timothyagevi@gmail.com>
2022-03-07 21:24:44 +0300

    check if segment isFinal

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -33,7 +33,7 @@ useEffect( () =>{
       setFormData({...formatDate,type:'Expense'})
     } else if(segment.intent.intent==='add_income'){
       setFormData( { ...formData,type:'Income'});
-    }
+    }else if( segment.isFinal && segment.intent.intent===)
  }
 },[segment])
 
