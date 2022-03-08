27140408e66c1d2f8d11db39b895c150ed3d57dc
timothyAgevi <timothyagevi@gmail.com>
2022-03-08 10:46:07 +0300

    add rafce

---
+++ src/components/Snackbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+import useStyles from './styles';
+
+const Snackbar = () => {
+  return (
+    <div>Snackbar</div>
+  )
+}
+
+export default Snackbar
