3f3858475d03ff7b589abe02b52e3d4072d1ad49
timothyAgevi <timothyagevi@gmail.com>
2022-03-08 10:20:08 +0300

    add callback to balance

--- src/context/context.js
+++ src/context/context.js
@@ -27,7 +27,7 @@ const [transactions, dispatch] = useReducer(contextReducer,initialState);//reduc
 
  const addTransaction= (transaction)=>{dispatch({ type:'ADD_TRASACTION',payload:transaction})};//err transactions
  
- const balance =transactions.reduce();
+ const balance =transactions.reduce( (acc,currVal)=>{ },0);
    //return jsx 
    return(
        <ExpenseTrackerContext.Provider value ={{
