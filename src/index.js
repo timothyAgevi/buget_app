ba9c03d80a728ec128da08b6143436f81ab3d411
timothyAgevi <timothyagevi@gmail.com>
2022-03-09 10:04:59 +0300

    add service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { SpeechProvider } from '@speechly/react-client';
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <SpeechProvider appId="07e558a3-d102-4cea-98d5-776b127f4100" language="en-US">
     <Provider>
@@ -14,3 +15,14 @@ ReactDOM.render(
   </SpeechProvider>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
