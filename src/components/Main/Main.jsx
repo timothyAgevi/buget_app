7e60c6e94a7f5d2424364373f8f6a786061c5923
timothyAgevi <timothyagevi@gmail.com>
2022-03-10 22:16:14 +0300

    update main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,7 +13,7 @@ const ExpenseTracker = () => {
 
   return (
     <Card className={classes.root}>
-      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardHeader title="Buget Manager" subheader="Powered by timothyAgevi" />
       <CardContent>
         <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
         <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
