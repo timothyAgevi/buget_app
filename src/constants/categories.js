adfcd0a97f8a1de65965941978a9d4b253986be5
timothyAgevi <timothyagevi@gmail.com>
2022-04-08 23:21:51 +0300

    improve  transaction categories

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,6 +1,7 @@
+//colors for each category
 const incomeColors = ['#123123', '#154731', '#165f40', '#16784f', '#14915f', '#10ac6e', '#0bc77e', '#04e38d', '#00ff9d'];
 const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#e57c58', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f'];
-
+//income categories
 export const incomeCategories = [
   { type: 'Business', amount: 0, color: incomeColors[0] },
   { type: 'Investments', amount: 0, color: incomeColors[1] },
@@ -12,7 +13,7 @@ export const incomeCategories = [
   { type: 'Savings', amount: 0, color: incomeColors[7] },
   { type: 'Rental income', amount: 0, color: incomeColors[8] },
 ];
-
+//expense categories
 export const expenseCategories = [
   { type: 'Bills', amount: 0, color: expenseColors[0] },
   { type: 'Car', amount: 0, color: expenseColors[1] },
