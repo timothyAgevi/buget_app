9471f1ec778f7f5daee4138a74b9067efb32ad19
timothyAgevi <timothyagevi@gmail.com>
2022-04-08 23:09:28 +0300

    restructured context

--- src/context/context.js
+++ src/context/context.js
@@ -18,7 +18,6 @@ export const Provider = ({ children }) => {
   };
 
   const balance = transactions.reduce((acc, currVal) => (currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount), 0);
-
   return (
     <ExpenseTrackerContext.Provider value={{
       transactions,
