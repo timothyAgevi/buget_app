d9ab16aa89d96c391fb69370e5c19c26acdeb584
timothyAgevi <timothyagevi@gmail.com>
2022-08-25 14:42:35 +0300

    removed service woker reportWebVitals

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { SpeechProvider } from '@speechly/react-client';
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+
+
 ReactDOM.render(
   <SpeechProvider appId="07e558a3-d102-4cea-98d5-776b127f4100" language="en-US">
     <Provider>
@@ -19,10 +19,10 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
 
